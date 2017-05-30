f8a08b5ae342204f60f789d9fc902a1811e46718
mcguffin <joern@flyingletters.com>
2017-05-30 15:29:21 +0200

    fix Relationship field column

--- include/ACFQuickEdit/Fields/PostObjectField.php
+++ include/ACFQuickEdit/Fields/PostObjectField.php
@@ -13,7 +13,24 @@ class PostObjectField extends RelationshipField {
 	public function render_input( $input_atts, $is_quickedit = true ) {
 		return '';
 	}
+	/**
+	 *	Render Column content
+	 *
+	 *	@param int|string $object_id
+	 *	@return string
+	 */
+	public function render_column( $object_id ) {
+		$value = get_field( $this->acf_field['key'], $object_id );
 
+		if ( ! $value ) {
+			return '';
+		}
+		$post = get_post( $value );
+		if ( current_user_can( 'edit_post', $value ) ) {
+			return sprintf('<a href="%s">%s</a>', get_edit_post_link( $value ), $post->post_title );
+		}
+		return sprintf('<a href="%s">%s</a>', get_permalink( $value ), $post->post_title );
 
+	}
 
 }
