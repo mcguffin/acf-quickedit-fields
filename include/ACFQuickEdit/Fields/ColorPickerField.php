b17cf5f54459fa7f93e80e29a720625b4219c997
mcguffin <joern@flyingletters.com>
2017-09-19 14:55:10 +0200

    color picker no-value

--- include/ACFQuickEdit/Fields/ColorPickerField.php
+++ include/ACFQuickEdit/Fields/ColorPickerField.php
@@ -12,12 +12,13 @@ class ColorPickerField extends Field {
 	 */
 	public function render_column( $object_id ) {
 		$value = get_field( $this->acf_field['key'], $object_id );
-		if ( $value ) {
-			$output = '<div class="color-indicator" style="border-radius:2px;border:1px solid #d2d2d2;width:26px;height:20px;background-color:'.$value.'"></div>';
-		} else {
-			$output = __('(No value)', 'acf-quick-edit-fields');
+		$indicator_class = 'color-indicator';
+		if ( ! $value ) {
+			$indicator_class .= ' no-value';
+			$value = 'rgba(255,255,255,0)';
 		}
-		return $output;
+
+		return sprintf( '<div class="%s" style="background-color:%s"></div>', $indicator_class, $value );
 
 	}
 
