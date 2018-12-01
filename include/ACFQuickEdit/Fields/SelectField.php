c3dc3bbec64ca9627ceaaf3f8a75a0811f99ca01
mcguffin <joern@flyingletters.com>
2018-12-01 13:47:16 +0100

    select multiple: don't render the '– Select –' option.

--- include/ACFQuickEdit/Fields/SelectField.php
+++ include/ACFQuickEdit/Fields/SelectField.php
@@ -27,7 +27,7 @@ class SelectField extends ChoiceField {
 
 		$output .= sprintf( '<select %s>', acf_esc_attr( $input_atts ) );
 
-		if ( $this->acf_field['allow_null'] ) {
+		if ( ! $this->acf_field['multiple'] && $this->acf_field['allow_null'] ) {
 			$output .= sprintf('<option value="">%s</option>', __( '— Select —', 'acf-quick-edit-fields' ) );
 		}
 
