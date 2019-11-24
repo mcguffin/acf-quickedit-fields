cff2319db61370a56c4c95cbd300edd56e82a025
DSUB <shout@m0n0mind.net>
2019-11-24 19:58:52 +0100

    Update post_object.js
    adjust select2 field args and set new select2 options from new values

--- src/js/lib/fields/post_object.js
+++ src/js/lib/fields/post_object.js
@@ -3,9 +3,33 @@ import $ from 'jquery';
 module.exports = {
 	type:'post_object',
 	initialize:function() {
-		this.parent().initialize.apply(this,arguments);
 
 		this.$input = this.$('select').prop( 'readonly', true );
+
+		this.parent().initialize.apply(this,arguments);
+
+		if( typeof acf !== 'object' ) {
+			return;
+		}
+		var self = this;
+
+		// init select2
+		this.$input.select2();		// 2DO: not really needed, but helps in showing placeholder text on init
+
+		// 2DO: workaround 'acf/fields/undefined/query' ajax action, but why?
+		acf.add_filter('select2_ajax_data', function( data, args, $input, $field ){
+			if( data.field_key !== self.key ) {
+				return data;
+			}
+			
+			let type = args.field.data.fieldType;
+			if( typeof type !== 'undefined' ) {
+				data.action = 'acf/fields/'+type+'/query';
+			}
+			
+			return data;	
+		});
+
 	},
 	setValue:function(value) {
 		// the value has been loded by an ajax request
@@ -17,7 +41,17 @@ module.exports = {
 		We'll have to fetch the corresponding post titles as well and append selected <option>s to this.$input
 		*/
 
-		this.$input.val(value);
+		var self = this;
+		if( _.isArray(value) ) {
+			_.each(value, function(val) {
+				self.$input.append( new Option(val.text, val.id, true, true) ).trigger('change');
+			});
+		} else if( _.isObject(value) ) {
+			// set current default value by appending a selected option element
+			self.$input.append( new Option(value.text, value.id, true, true) ).trigger('change');
+		}
+
 		return this;
 	}
 }
+
