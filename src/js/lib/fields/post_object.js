b351626b5f17be9f32dcffad63db797bca9f6bd6
mcguffin <joern@flyingletters.com>
2019-11-27 12:04:33 +0100

    post object: js init and setValue

--- src/js/lib/fields/post_object.js
+++ src/js/lib/fields/post_object.js
@@ -5,53 +5,30 @@ module.exports = {
 	initialize:function() {
 
 		this.$input = this.$('select').prop( 'readonly', true );
-
+		//
 		this.parent().initialize.apply(this,arguments);
-
-		if( typeof acf !== 'object' ) {
-			return;
-		}
-		var self = this;
-
-		// init select2
-		this.$input.select2();		// 2DO: not really needed, but helps in showing placeholder text on init
-
-		// 2DO: workaround 'acf/fields/undefined/query' ajax action, but why?
-		acf.add_filter('select2_ajax_data', function( data, args, $input, $field ){
-			if( data.field_key !== self.key ) {
-				return data;
-			}
-			
-			let type = args.field.data.fieldType;
-			if( typeof type !== 'undefined' ) {
-				data.action = 'acf/fields/'+type+'/query';
-			}
-			
-			return data;	
-		});
 
 	},
 	setValue:function(value) {
 		// the value has been loded by an ajax request
-		this.dntChanged( );
-		let acfField = new acf.models.PostObjectField( this.$input.closest('.acf-field') )
-		/*
-		`this.$input` points to the <select> jQuery object
-		`value` contains the selected post IDs.
-		We'll have to fetch the corresponding post titles as well and append selected <option>s to this.$input
-		*/
 
-		var self = this;
-		if( _.isArray(value) ) {
-			_.each(value, function(val) {
-				self.$input.append( new Option(val.text, val.id, true, true) ).trigger('change');
-			});
-		} else if( _.isObject(value) ) {
-			// set current default value by appending a selected option element
-			self.$input.append( new Option(value.text, value.id, true, true) ).trigger('change');
+		this.dntChanged( );
+
+		const self = this;
+		const acfField = new acf.models.PostObjectField( this.$input.closest('.acf-field') )
+		const append = item => {
+			self.$input.append( new Option( item.text, item.id, true, true ) );
 		}
+
+		if( _.isArray( value ) ) {
+			value.map( append )
+		} else if( _.isObject(value) ) {
+			append( value )
+		}
+
+		// do we need this ..?
+		// self.$input.trigger('change');
 
 		return this;
 	}
 }
-
